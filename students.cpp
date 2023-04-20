a2ee7fae4b03f2a2d42a4041b4c4d9f9096bc56b
Arma Burton <arma@armaburton.com>
2023-04-20 14:14:49 -0700

    app reads CSV file and collect the data in an array of structs

--- students.cpp
+++ students.cpp
@@ -23,7 +23,7 @@ typedef struct{
   char name[50];
   int age;
   double average;
-} Student
+} Student;
 
 int main(void){
   FILE *file;
@@ -45,13 +45,34 @@ int main(void){
                   &students[records].type,
                   students[records].name,
                   &students[records].age,
-                  &students[records].average
-                );
+                  &students[records].average);
 
+    if(read == 4)records++;
+
+    if(read != 4 && !feof(file)){
+      printf("File format incorrect.\n");
+      return 1;
+    }
+
+    if(ferror(file)){
+      printf("Error reading file.\n");
+      return 1;
+    }
 
   }while(!feof(file));
 
+  fclose(file);
 
+  printf("\n%d records read.\n\n", records);
+
+  for (int i = 0; i < records; i++){
+    printf("%c %s %d %.2f\n",
+           students[i].type, 
+           students[i].name, 
+           students[i].age, 
+           students[i].average);
+  }
+  printf("\n");
 
   return 0;
 }
